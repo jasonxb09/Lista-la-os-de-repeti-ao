7e1bda4ebec94729efdad706c1f4e58a71a19a0b
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-11-06 11:11:54 -0300

    Add files via upload

---
+++ pag141-f.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a,b,n;
+    printf("valores divisiveis por 4:\n");
+   for(a=0;a<=200;a++){
+        b = a%4;
+        if(b==0){
+            printf("%i\n",a);
+        }
+    }
+}
