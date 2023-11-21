8c337ba068995eb38f4dcd16f01b4d0a2c7ec943
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-11-21 08:15:45 -0300

    Add files via upload

---
+++ pag142-j.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float c, f;
+    c=10;
+    while(c<=100){
+        f=(c*9/5)+32;
+        printf("%.0f celsius = %.0f fahrenheit\n", c, f);
+        c=c+10;
+    }
+}
