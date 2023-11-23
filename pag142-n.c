36040c790066eef678e186dd94e99982665ac602
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-11-23 19:49:28 -0300

    Add files via upload

---
+++ pag142-n.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    float a, s, m, d;
+    s=0;
+    d=0;
+    printf("escreva um valor(para prosseguir com o algoritmo é necessario escrever um valor menor que 0:\n");
+    do
+    {
+        printf("escreva valor: ");
+        scanf("%f", &a);
+        if (a >= 0){
+            s=s+a;
+            d=d+1;
+        }
+    } while(a>=0);
+    if (d> 0){
+        m=s/d;
+        printf("\nsoma: %f\n", s);
+        printf("media: %f\n", m);
+        printf("total de valores lidos foi de: %.0f\n", d);
+    }
+}
+
