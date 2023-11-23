662ae465fd4c557d200346ffd9bb084c375e120d
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-11-23 19:37:18 -0300

    Update pag142-m.c

--- pag142-m.c
+++ pag142-m.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    int a, b , i;
-    b=0;
+    int v, s , i;
+    s=0;
     printf("escreva 10 valores.\n");
     for (i=1;i<=10;i++){
         printf("valor %i: ",i);
-        scanf("%i", &a);
-        b=b+a;
+        scanf("%i", &v);
+        s=s+v;
     }
 
-    printf("soma dos valores: %i\n", b);
-    printf("media dos valores: %i\n",b/10);
+    printf("soma dos valores: %i\n", s);
+    printf("media dos valores: %i\n",s/10);
 
 }
 
