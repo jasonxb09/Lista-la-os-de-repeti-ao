99cf34813faece7465e1339ae630a4218265a711
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-11-23 20:04:35 -0300

    Add files via upload

---
+++ pag143-s.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n1, n2, q;
+    q=0;
+    printf("escreva o dividendo: ");
+    scanf("%i", &n1);
+    printf("escreva o divisor: ");
+    scanf("%i", &n2);
+    while(n1>=n2){
+        n1 =n1-n2;
+        q=q+1;
+    }
+    printf("o quociente da divisao e: %i",q);
+}
